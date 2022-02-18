1afc0fea1e7b3d859fbf6d4941b788a74056213b
Kim Nguyen <kimnivore@gmail.com>
2022-02-17 17:16:25 -0800

    index & server done

--- api/server.js
+++ api/server.js
@@ -1,1 +1,30 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+const resourceRouter = require('./resource/router');
+const projectRouter = require('./project/router');
+const taskRouter = require('./task/router')
+
+const server = express();
+
+server.use(express.json());
+
+
+server.use('/api/resources', resourceRouter);
+server.use('/api/projects', projectRouter);
+server.use('/api/tasks', taskRouter);
+
+server.use('*', (req, res) => { //catchall needs to be at end
+    res.json({ api: 'up' })
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        customMessage: 'something went wrong inside the router',
+        message: err.message,
+        stack: err.stack,
+    });
+});
+
+
+module.exports = server;
