7bd06704787e9d216ca7569cefded2dd91a50b63
Kim Nguyen <kimnivore@gmail.com>
2022-02-18 14:10:25 -0800

    project booleans fleshed out

--- api/project/model.js
+++ api/project/model.js
@@ -1,14 +1,40 @@
 // build your `Project` model here
 const db = require('../../data/dbConfig');
+ 
+//select project_name, project_description, project_completed from projects;
+async function get() {
+    const rows = await db('projects')
+    .select('project_name', 'project_description', 'project_completed')
+   
+    const results = [];
 
-function get() {
-    return db('projects')
+    rows.forEach((row) => {
+        if(row.project_completed === 1) {
+            row.project_completed = true;
+        } else {
+            row.project_completed = false;
+        }
+        results.push(row);
+    });
+    return results
 }
 
 async function create(project) {
-   const [project_id] = await db('projects').insert(project);
-   return get().where({ project_id }).first();
+    const [project_id] = await db('projects').insert(project);
+    const row = await db('projects')
+        .where({ project_id })
+        .first();
+
+    const result = {
+        project_id: row.project_id,
+        project_name: row.project_name,
+        project_description: row.project_description,
+        project_completed: row.project_completed ? true : false,
+    }
+
+    return result;
 }
+
 
 module.exports = {
     get,
--- api/task/model.js
+++ api/task/model.js
@@ -1,16 +1,15 @@
 // build your `Task` model here
 const db = require('../../data/dbConfig');
 
+// select t.*, p.project_name, p.project_description from tasks as t
+// join projects as p on p.project_id = t.project_id
+// where t.task_id = 1
 function get() {
     return db('tasks as t')
     .join('projects as p', 'p.project_id', 't.project_id')
     .select('t.*', 'p.project_name', 'p.project_description')
-    // .where('t.task_id', task_id )
 }
 
-// select t.*, p.project_name, p.project_description from tasks as t
-// join projects as p on p.project_id = t.project_id
-// where t.task_id = 1
 async function create(task) {
     const [task_id] = await db('tasks').insert(task);
     return get().where({ task_id }).first();
