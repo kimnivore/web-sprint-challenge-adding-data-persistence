760949001d15aa0308b75a6ae3a4e9496aa09c6a
Kim Nguyen <kimnivore@gmail.com>
2022-02-18 11:04:13 -0800

    completed tasks except for boolean

--- api/task/model.js
+++ api/task/model.js
@@ -2,9 +2,15 @@
 const db = require('../../data/dbConfig');
 
 function get() {
-    return db('tasks')
+    return db('tasks as t')
+    .join('projects as p', 'p.project_id', 't.project_id')
+    .select('t.*', 'p.project_name', 'p.project_description')
+    // .where('t.task_id', task_id )
 }
 
+// select t.*, p.project_name, p.project_description from tasks as t
+// join projects as p on p.project_id = t.project_id
+// where t.task_id = 1
 async function create(task) {
     const [task_id] = await db('tasks').insert(task);
     return get().where({ task_id }).first();
