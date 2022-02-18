ee2abab5529696174029d3bf403049868ac417d6
Kim Nguyen <kimnivore@gmail.com>
2022-02-18 09:40:23 -0800

    completed routers

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,23 @@
 // build your `/api/projects` router here
+const express = require('express');
+const Project = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Project.get()
+        .then(projects => {
+            res.status(200).json(projects);
+    })
+        .catch(next)
+});
+
+router.post('/', (req, res, next) => {
+    Project.create(req.body)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(next)
+});
+
+module.exports = router;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,23 @@
 // build your `/api/tasks` router here
+const express = require('express');
+const Task = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Task.get()
+        .then(tasks => {
+            res.status(200).json(tasks);
+    })
+        .catch(next)
+});
+
+router.post('/', (req, res, next) => {
+    Task.create(req.body)
+        .then(task => {
+            res.status(201).json(task);
+        })
+        .catch(next);
+})
+
+module.exports = router
